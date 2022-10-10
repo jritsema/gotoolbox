bfeb4494beeb56686a71e330d8bcdff5b40b3fa9
John Ritsema <john_ritsema@yahoo.com>
2022-10-09 22:42:42 -0400

    adds HttpGetJSON (#3)

--- json.go
+++ json.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"net/http"
 	"os"
 )
 
@@ -12,17 +13,37 @@ func ReadJSONFile(fileName string) (map[string]string, error) {
 
 	jsonFile, err := os.Open(fileName)
 	if err != nil {
-		return nil, fmt.Errorf("could not open file: %v: %w", fileName, err)
+		return nil, fmt.Errorf("could not open file: %s: %w", fileName, err)
 	}
 	defer jsonFile.Close()
 	b, err := ioutil.ReadAll(jsonFile)
 	if err != nil {
-		return nil, fmt.Errorf("could not read file: %v : %w", fileName, err)
+		return nil, fmt.Errorf("could not read file: %s : %w", fileName, err)
 	}
 	var result map[string]string
 	err = json.Unmarshal(b, &result)
 	if err != nil {
-		return nil, fmt.Errorf("unmarshaling file: %v : %w", fileName, err)
+		return nil, fmt.Errorf("unmarshaling file: %s : %w", fileName, err)
 	}
 	return result, nil
+}
+
+// HttpGetJSON fetches the contents of the given URL
+// and decodes it as JSON into the given result,
+// which should be a pointer to the expected data.
+func HttpGetJSON(url string, result interface{}) error {
+
+	resp, err := http.Get(url)
+	if err != nil {
+		return fmt.Errorf("cannot fetch URL %q: %v", url, err)
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("unexpected http GET status: %s", resp.Status)
+	}
+	err = json.NewDecoder(resp.Body).Decode(result)
+	if err != nil {
+		return fmt.Errorf("cannot decode JSON: %v", err)
+	}
+	return nil
 }
